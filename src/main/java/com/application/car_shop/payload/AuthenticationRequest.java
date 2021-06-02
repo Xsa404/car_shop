394bc8d729bd72b3b2f00527b54c216919bf822d
Xsa404 <61477613+Xsa404@users.noreply.github.com>
2021-06-02 22:01:06 +0200

    AutenticationRequest.java gemaakt

---
+++ src/main/java/com/application/car_shop/payload/AuthenticationRequest.java
@@ -0,0 +1,35 @@
+package com.application.car_shop.payload;
+
+public class AuthenticationRequest {
+
+    private String username; //create username
+    private String password; //create password
+
+    //needed for request
+
+    public AuthenticationRequest() {
+    }
+
+    // create constructor
+
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters with Return statements for username and password
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
