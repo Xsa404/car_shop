9151326151de0ed6f89fb59d9a32a807124fa321
Xsa404 <61477613+Xsa404@users.noreply.github.com>
2021-06-06 20:05:58 +0200

    UserController gemaakt

--- src/main/java/com/application/car_shop/controller/UserController.java
+++ src/main/java/com/application/car_shop/controller/UserController.java
@@ -1,4 +1,69 @@
 package com.application.car_shop.controller;
 
+
+import com.application.car_shop.exception.BadRequest;
+import com.application.car_shop.model.ApplicationUser;
+import com.application.car_shop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/v1/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "")
+    public ResponseEntity<Object> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @GetMapping(value = "/{username}")
+    public ResponseEntity<Object> getUser(@PathVariable("username") String username) {
+        return ResponseEntity.ok().body(userService.getUser(username));
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<Object> createUser(@RequestBody ApplicationUser user) {
+        String newUsername = userService.createUser(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{username}")
+                .buildAndExpand(newUsername).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping(value = "/{username}")
+    public ResponseEntity<Object> updateUser(@PathVariable("username") String username, @RequestBody ApplicationUser user) {
+        userService.updateUser(username, user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/{username}")
+    public ResponseEntity<Object> deleteUser(@PathVariable("username") String username) {
+        userService.deleteUser(username);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{username}/authorities")
+    public ResponseEntity<Object> getUserAuthorities(@PathVariable("username") String username) {
+        return ResponseEntity.ok().body(userService.getAuthorities(username));
+    }
+
+    @PostMapping(value = "/{username}/authorities")
+    public ResponseEntity<Object> addUserAuthority(@PathVariable("username") String username, @RequestBody Map<String, Object> fields) {
+        try {
+            String authorityName = (String) fields.get("authority");
+            userService.addAuthority(username, authorityName);
+            return ResponseEntity.noContent().build();
+        } catch (Exception ex) {
+            throw new BadRequest();
+        }
+    }
 }
