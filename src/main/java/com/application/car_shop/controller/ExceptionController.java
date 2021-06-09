b1768415b936b18f0442f269697835898cce446f
Xsa404 <61477613+Xsa404@users.noreply.github.com>
2021-06-09 14:20:45 +0200

    ExceptionController gemaakt

--- src/main/java/com/application/car_shop/controller/ExceptionController.java
+++ src/main/java/com/application/car_shop/controller/ExceptionController.java
@@ -1,4 +1,18 @@
 package com.application.car_shop.controller;
 
+import com.application.car_shop.exception.RecordNotFound;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFound.class)
+    public ResponseEntity<Object> exception(RecordNotFound exception) {
+        return ResponseEntity.notFound().build();
+    }
 }
