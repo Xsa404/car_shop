adc96164fe5a138a5c370caeb7b10ce476d4f7c2
Xsa404 <61477613+Xsa404@users.noreply.github.com>
2021-06-11 22:37:32 +0200

    kleine wijzigingen aan opmaak en lay-out

--- src/main/java/com/application/car_shop/controller/UserController.java
+++ src/main/java/com/application/car_shop/controller/UserController.java
@@ -4,7 +4,6 @@ package com.application.car_shop.controller;
 import com.application.car_shop.exception.BadRequest;
 import com.application.car_shop.model.ApplicationUser;
 import com.application.car_shop.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,8 +15,11 @@ import java.util.Map;
 @RequestMapping(value = "/v1/users")
 public class UserController {
 
-    @Autowired // waarom doet deze het wel maar employee en customer niet?
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping(value = "")
     public ResponseEntity<Object> getUsers() {
